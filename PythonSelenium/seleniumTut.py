4248e1a238f46f1a18e26b79dd9666b5b49f2cd3
stefanobergamini <sbpoletto@hotmail.com>
2021-07-02 20:20:00 -0300

    Criado um código que pesquisa por selenium no github e retorna o titulo dos repositórios

--- PythonSelenium/seleniumTut.py
+++ PythonSelenium/seleniumTut.py
@@ -1,12 +1,35 @@
 #seguindo o tutorial
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.youtube.com/watch?v=Xjv1sY630Uc&ab_channel=TechWithTim")
+driver.get("https://www.github.com/")
 print(driver.title)
+driver.maximize_window()
 
-driver.quit()
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys("selenium")
+time.sleep(1)
+search_bar.send_keys(Keys.RETURN)
+
+try:
+    search_results = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "repo-list"))
+    )
+
+    titles = search_results.find_elements_by_class_name("mt-n1")
+    for title in titles:
+        name = title.find_element_by_class_name("v-align-middle")
+        print(name.text)
+finally:
+    print("The End")
+    driver.quit()
+
 
